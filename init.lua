1f273a634a9eebcac4c601f36533cc3c3d917101
Lon Sagisawa <lon@sagisawa.me>
2024-07-30 00:26:17 +0900

    map leader

--- init.lua
+++ init.lua
@@ -1,28 +1,31 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 -- lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- import plugin settings
-require('lazy').setup({
-    { import = 'plugins' },
+require("lazy").setup({
+	{ import = "plugins" },
 }, {})
 
 -- setup lsp
-require('mason-lspconfig').setup_handlers {
-    function (server_name)
-        require('lspconfig')[server_name].setup {}
-    end
-}
+require("mason-lspconfig").setup_handlers({
+	function(server_name)
+		require("lspconfig")[server_name].setup({})
+	end,
+})
 
 -- highlight on search
 vim.o.hlsearch = false
@@ -31,10 +34,10 @@ vim.o.hlsearch = false
 vim.wo.number = true
 
 -- mouse
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 
 -- clipboard
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = "unnamedplus"
 
 -- break indent
 vim.o.breakindent = true
@@ -47,17 +50,29 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 
 -- signcolumn
-vim.wo.signcolumn = 'yes'
+vim.wo.signcolumn = "yes"
 
 -- decrease update time
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 
 -- better completion
-vim.o.completeopt = 'menuone,noselect'
+vim.o.completeopt = "menuone,noselect"
 
 -- true color
 vim.o.termguicolors = true
 
 -- global statusline
 vim.o.laststatus = 3
+
+-- current line
+vim.o.cursorline = true
+
+-- space indent by default
+vim.o.tabstop = 4
+vim.o.expandtab = true
+
+-- exrc
+vim.o.exrc = true
+
+require("keymap")
