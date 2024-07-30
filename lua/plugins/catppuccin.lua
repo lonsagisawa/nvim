654bf384f93d2ea40dd7fd88e05b3cec0f636813
Lon Sagisawa <lon@sagisawa.me>
2024-07-31 02:04:00 +0900

    catppuccin integrations

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,7 +5,42 @@ return {
 	opts = {
 		flavour = "macchiato",
 		integrations = {
+			flash = true,
+			gitsigns = true,
+			markdown = true,
+			mason = true,
+			mini = {
+				enabled = true,
+			},
+			neotree = true,
 			noice = true,
+			native_lsp = {
+				enabled = true,
+				virtual_text = {
+					errors = { "italic" },
+					hints = { "italic" },
+					warnings = { "italic" },
+					information = { "italic" },
+					ok = { "italic" },
+				},
+				underlines = {
+					errors = { "underline" },
+					hints = { "underline" },
+					warnings = { "underline" },
+					information = { "underline" },
+					ok = { "underline" },
+				},
+				inlay_hints = {
+					background = true,
+				},
+			},
+			notify = true,
+			treesitter_context = true,
+			treesitter = true,
+			telescope = {
+				enabled = true,
+			},
+			which_key = true,
 		},
 	},
 	config = function()
