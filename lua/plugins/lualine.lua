2cdd993f061e2f149368a4241697250dc7fa7bda
Lon Sagisawa <lon@sagisawa.me>
2024-07-31 03:13:16 +0900

    lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,5 +3,18 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {
 		theme = "catppuccin",
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch" },
+			lualine_c = { "diff", "diagnostics" },
+			lualine_x = {},
+			lualine_y = { "filetype" },
+			lualine_z = { "location" },
+		},
+		extensions = {
+			"lazy",
+			"mason",
+			"neo-tree",
+		},
 	},
 }
