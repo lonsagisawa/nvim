bb4418321dcccf60018fb0c252fbcb5ac43be8f0
Lon Sagisawa <lon@sagisawa.me>
2024-08-02 04:17:41 +0900

    treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,10 +5,15 @@ return {
 		require("nvim-treesitter.configs").setup({
 			ensure_installed = "all",
 			auto_install = true,
-
+			sync_install = false,
+			ignore_install = {},
+			modules = {},
 			highlight = {
 				enable = true,
 				additional_vim_regex_highlighting = false,
+			},
+			indent = {
+				enable = true,
 			},
 		})
 	end,
