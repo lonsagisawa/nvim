b4f7371b01140a305ca5e6994558f3079b97c80f
Lon Sagisawa <lon@sagisawa.me>
2024-08-11 16:51:08 +0900

    mason-lspconfig: ensure_installed

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,3 +1,23 @@
 return {
 	"williamboman/mason-lspconfig.nvim",
+	opts = {
+		ensure_installed = {
+			"astro",
+			"cssls",
+			"css_variables",
+			"cssmodules_ls",
+			"docker_compose_language_service",
+			"dockerls",
+			"eslint",
+			"html",
+			"intelephense",
+			"lua_ls",
+			"marksman",
+			"svelte",
+			"tailwindcss",
+			"unocss",
+			"vtsls",
+			"volar",
+		},
+	},
 }
