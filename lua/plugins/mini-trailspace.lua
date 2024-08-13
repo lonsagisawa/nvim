7ef1f41dd7d38ba3c3785c2e09122ff16253ec53
Lon Sagisawa <lon@sagisawa.me>
2024-08-13 11:18:14 +0900

    auto trim trailing space

--- lua/plugins/mini-trailspace.lua
+++ lua/plugins/mini-trailspace.lua
@@ -2,4 +2,12 @@ return {
 	"echasnovski/mini.trailspace",
 	version = false,
 	opts = {},
+	config = function()
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			callback = function()
+				require("mini.trailspace").trim()
+				require("mini.trailspace").trim_last_lines()
+			end,
+		})
+	end,
 }
