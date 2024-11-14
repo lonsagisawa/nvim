5c054d3003d7cf178bb6a13922ec259347ccc1df
Lon Sagisawa <lon@sagisawa.me>
2024-11-14 19:28:40 +0900

    turn neo-tree to left

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -3,14 +3,13 @@ return {
 	branch = "v3.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
 	},
 	opts = {
 		enable_git_status = true,
 		hijack_netrw_behavior = "open_default",
 		window = {
-			position = "right",
+			position = "left",
 		},
 	},
 }
