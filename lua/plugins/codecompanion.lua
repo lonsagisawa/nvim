be7489cd9162d9616578e76f2eb7dbe5ea0d77e3
Lon Sagisawa <lon@sagisawa.me>
2025-02-02 18:41:15 +0900

    codecompanion setup for groq(failed)

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -3,9 +3,33 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-treesitter/nvim-treesitter",
-		"hrsh7th/nvim-cmp",                  -- Optional: For using slash commands and variables in the chat buffer
-		"nvim-telescope/telescope.nvim",     -- Optional: For using slash commands
+		"hrsh7th/nvim-cmp", -- Optional: For using slash commands and variables in the chat buffer
+		"nvim-telescope/telescope.nvim", -- Optional: For using slash commands
 		{ "stevearc/dressing.nvim", opts = {} }, -- Optional: Improves `vim.ui.select`
 	},
-	config = true
+	config = {
+		adapters = {
+			groq = function()
+				return require("codecompanion.adapters").extend("openai_compatible", {
+					env = {
+						url = "https://api.groq.com/openai",
+						api_key = "GROQ_API_KEY",
+					},
+					schema = {
+						model = {
+							default = "llama-3.3-70b-versatile",
+						},
+					},
+				})
+			end,
+		},
+		strategies = {
+			chat = {
+				adapter = "groq",
+			},
+			inline = {
+				adapter = "groq",
+			},
+		},
+	},
 }
