d8e07a914f362d329720c2e362dd0109bb38b804
Lon Sagisawa <lon@sagisawa.me>
2025-02-02 22:30:33 +0900

    stop treesitter install all by default

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 		build = ":TSUpdate",
 		config = function()
 			require("nvim-treesitter.configs").setup({
-				ensure_installed = "all",
+				ensure_installed = maintained,
 				auto_install = true,
 				sync_install = false,
 				ignore_install = {},
