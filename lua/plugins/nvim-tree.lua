cc99239471f66850b8ecb5efb0e4281ea730ab1f
Lon Sagisawa <lon@sagisawa.me>
2025-02-06 19:52:55 +0900

    keybind fix

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,4 +1,9 @@
 return {
 	"nvim-tree/nvim-tree.lua",
-	opts = {},
+	config = function()
+		require("nvim-tree").setup({})
+
+		vim.keymap.set({ "n", "x" }, "<leader>e", function() require("nvim-tree.api").tree.toggle() end,
+			{ desc = "Explorer" })
+	end,
 }
