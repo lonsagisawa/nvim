826637c6e2f131c967c6bcaadb4fa5162be9ed10
Lon Sagisawa <lon@sagisawa.me>
2025-02-08 18:00:26 +0900

    blink avante sources

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,6 +1,15 @@
 return {
 	"saghen/blink.cmp",
-	dependencies = "rafamadriz/friendly-snippets",
+
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+		{
+			"saghen/blink.compat",
+			lazy = true,
+			opts = {},
+		},
+	},
+
 	version = "*",
 
 	---@module 'blink.cmp'
@@ -17,8 +26,32 @@ return {
 		},
 
 		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
+			default = { "lsp", "path", "snippets", "buffer", "avante_commands", "avante_mentions", "avante_files" },
+
 			cmdline = {},
+
+			providers = {
+				avante_commands = {
+					name = "avante_commands",
+					module = "blink.compat.source",
+					score_offset = 90, -- show at a higher priority than lsp
+					opts = {},
+				},
+
+				avante_files = {
+					name = "avante_files",
+					module = "blink.compat.source",
+					score_offset = 100, -- show at a higher priority than lsp
+					opts = {},
+				},
+
+				avante_mentions = {
+					name = "avante_mentions",
+					module = "blink.compat.source",
+					score_offset = 1000, -- show at a higher priority than lsp
+					opts = {},
+				},
+			},
 		},
 
 		completion = {
