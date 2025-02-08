3343f23e610435bfd31a972a4123535e161ba29a
Lon Sagisawa <lon@sagisawa.me>
2025-02-08 18:44:38 +0900

    enable several mini.lua components, replace nvim-tree with mini.files

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,6 +4,18 @@ return {
 	config = function()
 		require("mini.cursorword").setup()
 		require("mini.trailspace").setup()
+		require("mini.splitjoin").setup()
+		require("mini.icons").setup()
+		require("mini.ai").setup()
+		require("mini.surround").setup()
+
+		require("mini.files").setup({
+			mappings = {
+				close = "<ESC>",
+				go_in_plus = "l",
+				go_out_plus = "h",
+			},
+		})
 
 		vim.api.nvim_create_autocmd("BufWritePre", {
 			callback = function()
@@ -12,4 +24,13 @@ return {
 			end,
 		})
 	end,
+	keys = {
+		{
+			"<leader>e",
+			function()
+				MiniFiles.open()
+			end,
+			desc = "Explorer",
+		},
+	},
 }
--- lua/plugins/nvim-tree.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"nvim-tree/nvim-tree.lua",
-	config = function()
-		require("nvim-tree").setup({})
-
-		vim.keymap.set({ "n", "x" }, "<leader>e", function() require("nvim-tree.api").tree.toggle() end,
-			{ desc = "Explorer" })
-	end,
-}
