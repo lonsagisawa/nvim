858b49856ec232a6f7cc567e695e747550d8ed46
Lon Sagisawa <lon@sagisawa.me>
2025-02-26 16:44:34 +0900

    improve mini.pairs

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -36,7 +36,7 @@ return {
 				["["] = {
 					action = "open",
 					pair = "[]",
-					neigh_pattern = ".[%s%z%\")}%]]",
+					neigh_pattern = ".[%s%z%>\")}%]]",
 					register = { cr = false },
 					-- foo|bar -> press "[" -> foo[bar
 					-- foobar| -> press "[" -> foobar[]
@@ -52,7 +52,7 @@ return {
 					action = "open",
 					pair = "{}",
 					-- neigh_pattern = ".[%s%z%)}]",
-					neigh_pattern = ".[%s%z%\")}%]]",
+					neigh_pattern = ".[%s%z%>\")}%]]",
 					register = { cr = false },
 					-- foo|bar -> press "{" -> foo{bar
 					-- foobar| -> press "{" -> foobar{}
@@ -67,7 +67,7 @@ return {
 					action = "open",
 					pair = "()",
 					-- neigh_pattern = ".[%s%z]",
-					neigh_pattern = ".[%s%z%\")]",
+					neigh_pattern = ".[%s%z%>\")]",
 					register = { cr = false },
 					-- foo|bar -> press "(" -> foo(bar
 					-- foobar| -> press "(" -> foobar()
