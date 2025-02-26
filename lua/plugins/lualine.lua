936e3ea55fc8f1a1bc1cbecb973ea26e5ebe51a3
Lon Sagisawa <lon@sagisawa.me>
2025-02-26 12:23:14 +0900

    show ls names

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,52 +1,64 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {
-		options = {
-			theme = "catppuccin",
-			section_separators = {
-				left = "",
-				right = "",
-			},
-			component_separators = "",
-			globalstatus = true,
-			disabled_filetypes = {
-				winbar = {
-					"NvimTree",
-					"no-neck-pain",
+	config = function()
+		-- https://qiita.com/uhooi/items/99aeff822d4870a8e269
+		local component__lsp_names = function()
+			local clients = vim.iter(vim.lsp.get_clients({ bufnr = 0 }))
+				:map(function(client)
+					return client.name
+				end)
+				:totable()
+			return " " .. table.concat(clients, ", ")
+		end
+
+		require("lualine").setup({
+			options = {
+				theme = "catppuccin",
+				section_separators = {
+					left = "",
+					right = "",
+				},
+				component_separators = "",
+				globalstatus = true,
+				disabled_filetypes = {
+					winbar = {
+						"NvimTree",
+						"no-neck-pain",
+					},
 				},
 			},
-		},
-		sections = {
-			lualine_a = { "mode" },
-			lualine_b = { "branch" },
-			lualine_c = {},
-			lualine_x = {},
-			lualine_y = { "diff", "diagnostics" },
-			lualine_z = { "location", "progress" },
-		},
-		extensions = {
-			"lazy",
-			"mason",
-			"nvim-tree",
-		},
-		winbar = {
-			lualine_b = {
-				{ "aerial", sep = "  " },
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", component__lsp_names },
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = { "diff", "diagnostics" },
+				lualine_z = { "location", "progress" },
 			},
-			lualine_z = {
-				{ "filetype", padding = { left = 1, right = 0 }, icon_only = true },
-				{ "filename", padding = { left = 0, right = 1 } },
+			extensions = {
+				"lazy",
+				"mason",
+				"nvim-tree",
 			},
-		},
-		inactive_winbar = {
-			lualine_b = {
-				{ "aerial", sep = "  " },
+			winbar = {
+				lualine_b = {
+					{ "aerial", sep = "  " },
+				},
+				lualine_z = {
+					{ "filetype", padding = { left = 1, right = 0 }, icon_only = true },
+					{ "filename", padding = { left = 0, right = 1 } },
+				},
 			},
-			lualine_z = {
-				{ "filetype", padding = { left = 1, right = 0 }, icon_only = true },
-				{ "filename", padding = { left = 0, right = 1 } },
+			inactive_winbar = {
+				lualine_b = {
+					{ "aerial", sep = "  " },
+				},
+				lualine_z = {
+					{ "filetype", padding = { left = 1, right = 0 }, icon_only = true },
+					{ "filename", padding = { left = 0, right = 1 } },
+				},
 			},
-		},
-	},
+		})
+	end,
 }
