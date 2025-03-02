49be0c874278c71b243f82cbf2550fb03c4c08cf
Lon Sagisawa <lon@sagisawa.me>
2025-03-02 13:42:48 +0900

    introduce colorful-menu for blink.cmp

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -8,6 +8,7 @@ return {
 			lazy = true,
 			opts = {},
 		},
+		"xzbdmw/colorful-menu.nvim",
 	},
 
 	version = "*",
@@ -53,6 +54,28 @@ return {
 		},
 
 		completion = {
+			menu = {
+				auto_show = true,
+
+				draw = {
+					columns = {
+						{ "kind_icon" },
+						{ "label",    gap = 1 },
+					},
+
+					components = {
+						label = {
+							text = function(ctx)
+								return require("colorful-menu").blink_components_text(ctx)
+							end,
+							highlight = function(ctx)
+								return require("colorful-menu").blink_components_highlight(ctx)
+							end,
+						}
+					},
+				}
+			},
+
 			documentation = {
 				auto_show = true,
 				auto_show_delay_ms = 300,
