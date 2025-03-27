8cf1e8bd558496ca04cd9facbcdf1e3efb54bdbb
Lon Sagisawa <lon@sagisawa.me>
2025-03-27 15:13:11 +0900

    dial: configuration

--- lua/plugins/dial.lua
+++ lua/plugins/dial.lua
@@ -1,13 +1,40 @@
 return {
 	"monaqa/dial.nvim",
+	-- stylua: ignore
 	keys = {
-		{ "<C-a>", function () require("dial.map").manipulate("increment", "normal") end, "n" },
-		{ "<C-x>", function () require("dial.map").manipulate("decrement", "normal") end, "n" },
-		{ "g<C-a>", function () require("dial.map").manipulate("increment", "gnormal") end, "n" },
-		{ "g<C-x>", function () require("dial.map").manipulate("decrement", "gnormal") end, "n" },
-		{ "<C-a>", function () require("dial.map").manipulate("increment", "visual") end, "v" },
-		{ "<C-x>", function () require("dial.map").manipulate("decrement", "visual") end, "v" },
-		{ "g<C-a>", function () require("dial.map").manipulate("increment", "gvisual") end, "v" },
-		{ "g<C-x>", function () require("dial.map").manipulate("decrement", "gvisual") end, "v" },
+		{ "<C-a>", function () require("dial.map").manipulate("increment", "normal") end, mode = "n" },
+		{ "<C-x>", function () require("dial.map").manipulate("decrement", "normal") end, mode = "n" },
+		{ "g<C-a>", function () require("dial.map").manipulate("increment", "gnormal") end, mode = "n" },
+		{ "g<C-x>", function () require("dial.map").manipulate("decrement", "gnormal") end, mode = "n" },
+		{ "<C-a>", function () require("dial.map").manipulate("increment", "visual") end, mode = "v" },
+		{ "<C-x>", function () require("dial.map").manipulate("decrement", "visual") end, mode = "v" },
+		{ "g<C-a>", function () require("dial.map").manipulate("increment", "gvisual") end, mode = "v" },
+		{ "g<C-x>", function () require("dial.map").manipulate("decrement", "gvisual") end, mode = "v" },
 	},
+	config = function()
+		local augend = require("dial.augend")
+
+		require("dial.config").augends:register_group({
+			default = {
+				augend.integer.alias.decimal,
+				augend.integer.alias.hex,
+				augend.constant.alias.bool,
+				augend.constant.new({
+					elements = { "&&", "||" },
+					word = false,
+					cyclic = true,
+				}),
+				augend.constant.new({
+					elements = { "==", "!=" },
+					word = false,
+					cyclic = true,
+				}),
+				augend.constant.new({
+					elements = { "===", "!==" },
+					word = false,
+					cyclic = true,
+				}),
+			},
+		})
+	end,
 }
