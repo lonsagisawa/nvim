146071893c30e9ea24853d9b077c0520f0b1b363
Lon Sagisawa <lon@sagisawa.me>
2025-04-15 12:43:41 +0900

    lsp: going more native

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,84 +1,87 @@
-local blink_capabilities = require('blink.cmp').get_lsp_capabilities()
+local blink_capabilities = require("blink.cmp").get_lsp_capabilities()
 
-require("mason").setup({})
+function mergeTables(t1, t2)
+	local result = {}
+	for i = 1, #t1 do
+		result[#result + 1] = t1[i]
+	end
+	for i = 1, #t2 do
+		result[#result + 1] = t2[i]
+	end
+	return result
+end
+
+local servers = {
+	"cssls",
+	"css_variables",
+	"cssmodules_ls",
+	"docker_compose_language_service",
+	"dockerls",
+	"html",
+	"intelephense",
+	"lua_ls",
+	"marksman",
+	"svelte",
+	"unocss",
+	"vtsls",
+}
+
+-- https://github.com/neovim/nvim-lspconfig/issues/3705
+local legacy_servers = {
+	"astro",
+	"tailwindcss",
+	"volar",
+}
+
+require("mason").setup()
 require("mason-lspconfig").setup({
-	ensure_installed = {
-		"astro",
-		"cssls",
-		"css_variables",
-		"cssmodules_ls",
-		"docker_compose_language_service",
-		"dockerls",
-		"eslint",
-		"html",
-		"intelephense",
-		"lua_ls",
-		"marksman",
-		"svelte",
-		"tailwindcss",
-		"unocss",
-		"vtsls",
-		"volar",
+	ensure_installed = mergeTables(servers, legacy_servers),
+})
+
+vim.lsp.config("html", {
+	filetypes = { "html", "blade" },
+})
+
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
 	},
 })
 
-require("mason-lspconfig").setup_handlers({
-	function(server_name)
-		require("lspconfig")[server_name].setup({
-			capabilities = blink_capabilities,
-		})
-	end,
-
-	["html"] = function()
-		vim.lsp.config("html", {
-			filetypes = { "html", "blade" },
-		})
-
-		vim.lsp.enable("html")
-	end,
-
-	["lua_ls"] = function()
-		vim.lsp.config("lua_ls", {
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" },
-					},
-				},
-			},
-		})
-
-		vim.lsp.enable("lua_ls")
-	end,
-
-	["vtsls"] = function()
-		require("lspconfig").vtsls.setup({
-			filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-			settings = {
-				vtsls = { tsserver = { globalPlugins = {} } },
-			},
-			capabilities = blink_capabilities,
-			before_init = function(params, config)
-				local result = vim.system(
-						{ "npm", "query", "#vue" },
-						{ cwd = params.workspaceFolders[1].name, text = true }
-					)
-					:wait()
-				if result.stdout ~= "[]" then
-					local vuePluginConfig = {
-						name = "@vue/typescript-plugin",
-						location = require("mason-registry").get_package("vue-language-server"):get_install_path()
-							.. "/node_modules/@vue/language-server",
-						languages = { "vue" },
-						configNamespace = "typescript",
-						enableForWorkspaceTypeScriptVersions = true,
-					}
-					table.insert(config.settings.vtsls.tsserver.globalPlugins, vuePluginConfig)
-				end
-			end,
-		})
+vim.lsp.config("vtsls", {
+	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+	settings = {
+		vtsls = { tsserver = { globalPlugins = {} } },
+	},
+	capabilities = blink_capabilities,
+	before_init = function(params, config)
+		local result = vim.system({ "npm", "query", "#vue" }, { cwd = params.workspaceFolders[1].name, text = true })
+			:wait()
+		if result.stdout ~= "[]" then
+			local vuePluginConfig = {
+				name = "@vue/typescript-plugin",
+				location = require("mason-registry").get_package("vue-language-server"):get_install_path()
+					.. "/node_modules/@vue/language-server",
+				languages = { "vue" },
+				configNamespace = "typescript",
+				enableForWorkspaceTypeScriptVersions = true,
+			}
+			table.insert(config.settings.vtsls.tsserver.globalPlugins, vuePluginConfig)
+		end
 	end,
 })
+
+vim.lsp.enable(servers)
+
+for _, server_name in ipairs(legacy_servers) do
+	require("lspconfig")[server_name].setup({
+		capabilities = blink_capabilities
+	})
+end
 
 vim.lsp.enable({
 	"laravel-ls",
@@ -92,8 +95,7 @@ vim.api.nvim_create_autocmd("LspProgress", {
 	---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
 	callback = function(ev)
 		local client = vim.lsp.get_client_by_id(ev.data.client_id)
-		local value = ev.data.params
-			.value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
+		local value = ev.data.params.value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
 		if not client or type(value) ~= "table" then
 			return
 		end
