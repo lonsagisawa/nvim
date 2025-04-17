bba851e5ad095d3fc53439e16f02a7c287b643c4
Lon Sagisawa <lon@sagisawa.me>
2025-04-18 02:52:42 +0900

    lsp: required options for astro ls

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -74,11 +74,27 @@ vim.lsp.config("vtsls", {
 	end,
 })
 
+local mason_tsdk_path = require("mason-registry").get_package("vtsls"):get_install_path()
+				.. "/node_modules/@vtsls/language-server/node_modules/typescript/lib"
+
 vim.lsp.config("volar", {
 	init_options = {
 		typescript = {
-			tsdk = require("mason-registry").get_package("vtsls"):get_install_path()
-				.. "/node_modules/@vtsls/language-server/node_modules/typescript/lib",
+			tsdk = mason_tsdk_path
+		},
+	},
+	on_new_config = function(new_config, new_root_dir)
+		local lib_path = vim.fs.find("node_modules/typescript/lib", { path = new_root_dir, upward = true })[1]
+		if lib_path then
+			new_config.init_options.typescript.tsdk = lib_path
+		end
+	end,
+})
+
+vim.lsp.config("astro", {
+	init_options = {
+		typescript = {
+			tsdk = mason_tsdk_path
 		},
 	},
 	on_new_config = function(new_config, new_root_dir)
