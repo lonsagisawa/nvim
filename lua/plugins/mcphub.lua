025db5ac71abb9de60717599311bc21a2e303519
Lon Sagisawa <lon@sagisawa.me>
2025-05-06 12:01:06 +0900

    mcphub: slash commands

--- lua/plugins/mcphub.lua
+++ lua/plugins/mcphub.lua
@@ -1,12 +1,17 @@
 return {
-    "ravitemer/mcphub.nvim",
-    -- comment the following line to ensure hub will be ready at the earliest
-    cmd = "MCPHub",  -- lazy load by default
-    build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
-    config = function()
-      require("mcphub").setup({
+	"ravitemer/mcphub.nvim",
+	-- comment the following line to ensure hub will be ready at the earliest
+	cmd = "MCPHub", -- lazy load by default
+	build = "bundled_build.lua", -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
+	config = function()
+		require("mcphub").setup({
 			auto_approve = false,
 			use_bundled_binary = true,
+			extensions = {
+				avante = {
+					make_slash_commands = true, -- make /slash commands from MCP server prompts
+				},
+			},
 		})
-    end,
+	end,
 }
