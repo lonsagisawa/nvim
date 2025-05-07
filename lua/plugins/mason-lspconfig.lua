d5dd9e354018d826c68a6972defdbcc7526c1c7e
Lon Sagisawa <lon@sagisawa.me>
2025-05-07 12:41:42 +0900

    mason: pin to v1 until I can fix breakage

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,4 +1,5 @@
 return {
 	"williamboman/mason-lspconfig.nvim",
+	version = "v1.32.0",
 	-- 設定をここに書かない！config/lsp.luaにまとめて書いている
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,5 @@
 return {
 	"williamboman/mason.nvim",
+	version = "v1.11.0",
 	-- 設定をここに書かない！config/lsp.luaにまとめて書いている
 }
