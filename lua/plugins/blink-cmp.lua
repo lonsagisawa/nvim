4947e46a3d2ce7b2b7714b85dfb55721fb7531a9
Lon Sagisawa <lon@sagisawa.me>
2025-05-12 13:26:33 +0900

    blink.cmp: properly configure lazydev

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -68,7 +68,15 @@ return {
 		},
 
 		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
+			default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+
+			providers = {
+				lazydev = {
+					name = "LazyDev",
+					module = "lazydev.integrations.blink",
+					score_offset = 100,
+				},
+			},
 		},
 
 		fuzzy = { implementation = "prefer_rust_with_warning" },
