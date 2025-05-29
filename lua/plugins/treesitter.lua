cbf5bfcecd5f6d951d7b2bd02e6de27f80a209d3
Lon Sagisawa <lon@sagisawa.me>
2025-05-29 10:25:41 +0900

    treesitter: switch nvim-treesitter to main

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,26 +2,28 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
+		branch = "main",
 		lazy = true,
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = maintained,
-				auto_install = true,
-				sync_install = false,
-				ignore_install = {},
-				modules = {},
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = false,
-				},
-				indent = {
-					enable = true,
-				},
-			})
-		end,
+		-- config = function()
+		-- 	require("nvim-treesitter.configs").setup({
+		-- 		ensure_installed = maintained,
+		-- 		auto_install = true,
+		-- 		sync_install = false,
+		-- 		ignore_install = {},
+		-- 		modules = {},
+		-- 		highlight = {
+		-- 			enable = true,
+		-- 			additional_vim_regex_highlighting = false,
+		-- 		},
+		-- 		indent = {
+		-- 			enable = true,
+		-- 		},
+		-- 	})
+		-- end,
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-textobjects",
 		event = "VeryLazy",
+		branch = "main",
 	},
 }
