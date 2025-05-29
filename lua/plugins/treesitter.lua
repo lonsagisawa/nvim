73f8c73c0c911cdbdf15c400dfc9229d3dfab5d0
Lon Sagisawa <lon@sagisawa.me>
2025-05-29 10:33:45 +0900

    treesitter: nvim-treesitter does not support lazy-loading

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,23 +3,7 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		branch = "main",
-		lazy = true,
-		-- config = function()
-		-- 	require("nvim-treesitter.configs").setup({
-		-- 		ensure_installed = maintained,
-		-- 		auto_install = true,
-		-- 		sync_install = false,
-		-- 		ignore_install = {},
-		-- 		modules = {},
-		-- 		highlight = {
-		-- 			enable = true,
-		-- 			additional_vim_regex_highlighting = false,
-		-- 		},
-		-- 		indent = {
-		-- 			enable = true,
-		-- 		},
-		-- 	})
-		-- end,
+		lazy = false,
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-textobjects",
