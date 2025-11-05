af8325897682a474f40a75624910cfb69d1239be
Lon Sagisawa <lon@sagisawa.me>
2025-11-05 13:02:11 +0900

    incline: introduce

---
+++ lua/plugins/incline.lua
@@ -0,0 +1,31 @@
+return {
+	"b0o/incline.nvim",
+	opts = {
+		window = {
+			padding = 0,
+			margin = { horizontal = 0 },
+		},
+		render = function(props)
+			local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+			if filename == "" then
+				filename = "[No Name]"
+			end
+			local ft_icon, ft_color = require("nvim-web-devicons").get_icon_color(filename)
+			local modified = vim.bo[props.buf].modified
+			return {
+				ft_icon and {
+					" ",
+					ft_icon,
+					" ",
+					guibg = ft_color,
+					guifg = require("incline.helpers").contrast_color(ft_color),
+				} or "",
+				" ",
+				{ filename, gui = modified and "bold,italic" or "bold" },
+				" ",
+				guibg = require("catppuccin.palettes").get_palette("mocha").surface0,
+			}
+		end,
+	},
+	event = "VeryLazy",
+}
