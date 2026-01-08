b514d8b5990576e8e57f23b9381d24ce1c1cd1e4
Lon Sagisawa <lon@lonsagisawa.jp>
2026-01-08 15:03:26 +0900

    lazy.nvim workaround

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,5 +27,15 @@ require("lazy").setup({
 	},
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+	desc = "User: fix backdrop for lazy window",
+	pattern = "lazy_backdrop",
+	group = vim.api.nvim_create_augroup("lazynvim-fix", { clear = true }),
+	callback = function(ctx)
+		local win = vim.fn.win_findbuf(ctx.buf)[1]
+		vim.api.nvim_win_set_config(win, { border = "none" })
+	end,
+})
+
 -- Open lazy.nvim
 vim.keymap.set({ "n", "x" }, "<leader>ll", require("lazy").show, { desc = "Open lazy.nvim" })
