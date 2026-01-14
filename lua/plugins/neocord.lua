d6012aa824cb7f87ec938526c88b6e2d50ec0432
Lon Sagisawa <lon@lonsagisawa.jp>
2026-01-14 18:49:18 +0900

    remove neocode, no-neck-pain

--- lua/plugins/neocord.lua
+++
@@ -1,1 +0,0 @@
-MiniDeps.add({ source = "IogaMaster/neocord" })
--- lua/plugins/no-neck-pain.lua
+++
@@ -1,9 +0,0 @@
-MiniDeps.add({ source = "shortcuts/no-neck-pain.nvim" })
-
-require("no-neck-pain").setup({
-	width = 160,
-	autocmds = {
-		enableOnVimEnter = false,
-		skipEnteringNoNeckPainBuffer = true,
-	},
-})
