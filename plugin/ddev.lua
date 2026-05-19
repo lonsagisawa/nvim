86446e41f1b5e07186242a751735823cf084ff73
Lon Sagisawa <lon@lonsagisawa.jp>
2026-05-19 11:19:41 +0900

    ddev: keybind

--- plugin/ddev.lua
+++ plugin/ddev.lua
@@ -1,1 +1,7 @@
-require("ddev").setup()
+local later = function(f) require("mini.misc").safely('later', f) end
+
+later(function()
+	require("ddev").setup()
+
+	vim.keymap.set("n", "<leader>dd", function() require("ddev.actions").open_picker() end)
+end)
